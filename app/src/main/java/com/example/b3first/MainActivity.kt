c0235c8ab19017d8c753022537b64a418d552f08
abdul ansari <abdul.tanveer@gmail.com>
2022-08-31 23:57:05 +0530

    lifecycle of an actiivty

--- app/src/main/java/com/example/b3first/MainActivity.kt
+++ app/src/main/java/com/example/b3first/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.b3first
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.EditText
 import android.widget.TextView
@@ -14,13 +15,48 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var etName: EditText //declaration
     lateinit var conTextView: TextView
+    var TAG = MainActivity::class.java.simpleName
 
+    /**
+     * memory is allocated for the activity
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         etName = findViewById(R.id.etName) // instantiation
         conTextView = findViewById(R.id.tvContact)
+        Log.i(TAG,"onCreate")
+
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(TAG,"onstart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.v(TAG,"onResume--restore the state")
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.w(TAG,"onPause-- save state app")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG,"onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.e(TAG,"onDestroy-- release the resources")
+
+    }
+
+
 
     fun clickHandler(viewClicked: View) {
         when(viewClicked.id){

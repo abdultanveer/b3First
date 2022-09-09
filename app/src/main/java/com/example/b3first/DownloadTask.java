993f6be0f234a3f4c5939744c2af1c5f9abb071f
abdul ansari <abdul.tanveer@gmail.com>
2022-09-09 22:18:35 +0530

    asynctask part-4

--- app/src/main/java/com/example/b3first/DownloadTask.java
+++ app/src/main/java/com/example/b3first/DownloadTask.java
@@ -3,6 +3,7 @@ package com.example.b3first;
 import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.util.Log;
+import android.view.View;
 import android.widget.ProgressBar;
 
 //string =url i/p type Integer = percentage - progress type, Bitmap -- image- resulttype
@@ -14,13 +15,18 @@ public class DownloadTask extends AsyncTask<String,Integer, Bitmap> {
        progressBar = mProgressBar;
    }
 
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        progressBar.setVisibility(View.VISIBLE);
+    }
 
     @Override//will run on the background worker thread
     protected Bitmap doInBackground(String... url) {
         Log.i(TAG,"doInBackground--"+url[0]);
         for(int i=0; i<100;i++) {
             try {
-                Thread.sleep(100);
+                Thread.sleep(50);
                 publishProgress(i);
             } catch (InterruptedException e) {
                 e.printStackTrace();
@@ -33,5 +39,11 @@ public class DownloadTask extends AsyncTask<String,Integer, Bitmap> {
     protected void onProgressUpdate(Integer... values) {
         super.onProgressUpdate(values);
         progressBar.setProgress(values[0]);
+    }
+
+    @Override
+    protected void onPostExecute(Bitmap bitmap) {
+        super.onPostExecute(bitmap);
+        progressBar.setVisibility(View.INVISIBLE);
     }
 }

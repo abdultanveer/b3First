5ae062ea2da10ad8f7bd6c06d67d3d4883223fa8
abdul ansari <abdul.tanveer@gmail.com>
2022-09-14 21:08:00 +0530

    notifications

--- app/src/main/java/com/example/b3first/MainActivity.kt
+++ app/src/main/java/com/example/b3first/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.b3first
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -11,6 +16,8 @@ import android.widget.ProgressBar
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 
 
 class MainActivity : AppCompatActivity() {
@@ -61,8 +68,53 @@ class MainActivity : AppCompatActivity() {
             R.id.btnDownload -> {downloadImage()}
             R.id.btnCal -> {openCalendar()}
             R.id.btnIntent -> { handleIntent()}
+            R.id.btnNotification -> { showNotification()}
         }
     }
+
+    private fun showNotification() {
+        createNotificationChannel()
+        val intent = Intent(this, HomeActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+
+        //pending intent is an intent fired by the os at later point in time on behalf of ur app
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+
+
+        var builder = NotificationCompat.Builder(this, "CHANNEL_ID")
+            .setSmallIcon(com.google.android.material.R.drawable.ic_clock_black_24dp)
+            .setContentTitle("cognizant android")
+            .setContentText("batch 3 ")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+
+        with(NotificationManagerCompat.from(this)) {
+            // notificationId is a unique int for each notification that you must define
+            notify(123, builder.build())
+        }
+
+    }
+
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "CHANNEL_ID"
+                //getString(R.string.channel_name)
+            val descriptionText = "description of notification channel- cognizant"
+                //getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel("CHANNEL_ID", name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
 
     private fun handleIntent() {
         var mIntent = Intent(Intent.ACTION_VIEW,Uri.parse("http:yahoo.com"))

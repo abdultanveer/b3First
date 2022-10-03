f67df4010679adac44a9262d6cf67caedd0e9a9f
abdul ansari <abdul.tanveer@gmail.com>
2022-10-04 00:32:31 +0530

    mockito

---
+++ app/src/test/java/com/example/b3first/ServiceTest.java
@@ -0,0 +1,40 @@
+package com.example.b3first;
+
+
+
+import com.example.b3first.mockito.Database;
+import com.example.b3first.mockito.Service;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+
+@ExtendWith(MockitoExtension.class)
+class ServiceTest {
+
+    @Mock
+    Database databaseMock;
+
+   // Database database = new Database();
+
+    @Test
+    public void testQuery()  {
+        assertNotNull(databaseMock);
+        when(databaseMock.isAvailable()).thenReturn(true);
+        Service t  = new Service(databaseMock);
+
+        boolean check = t.query("* from t");
+        assertTrue(check);
+
+
+
+
+    }
+
+    }
